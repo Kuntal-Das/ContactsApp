83fdcf7b41c66bb3e674722777100dda1fc98ed0
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-26 23:30:56 +0530

    RelayCommand

---
+++ ContactsApp/Command/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ContactsApp.Command
+{
+    public class RelayCommand : ICommand
+    {
+        Action<object> executeAction;
+        Func<object, bool> canExecute;
+        bool canExecuteCache;
+        public RelayCommand(Action<object> executeAction, Func<object, bool> canExecute, bool canExecuteCache)
+        {
+            this.canExecute = canExecute;
+            this.executeAction = executeAction;
+            this.canExecuteCache = canExecuteCache;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter) => canExecute == null ? true : canExecute(parameter);
+
+        public void Execute(object parameter) => executeAction(parameter);
+    }
+}
