7e832a5f79512c17d15706390d328b909e19ed7a
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-27 10:01:23 +0530

    generic RelayCommand

--- ContactsApp/Command/RelayCommand.cs
+++ ContactsApp/Command/RelayCommand.cs
@@ -7,25 +7,54 @@ using System.Windows.Input;
 
 namespace ContactsApp.Command
 {
-    public class RelayCommand : ICommand
+    public class RelayCommand<T> : ICommand
     {
-        Action<object> executeAction;
-        Func<object, bool> canExecute;
+        Action<T> executeAction;
+        Func<T, bool> canExecute;
         bool canExecuteCache;
-        public RelayCommand(Action<object> executeAction, Func<object, bool> canExecute, bool canExecuteCache)
+        public RelayCommand(Action<T> executeAction, Func<T, bool> canExecute, bool canExecuteCache)
         {
             this.canExecute = canExecute;
             this.executeAction = executeAction;
             this.canExecuteCache = canExecuteCache;
         }
-        public event EventHandler CanExecuteChanged
+
+        public event EventHandler? CanExecuteChanged
         {
             add => CommandManager.RequerySuggested += value;
             remove => CommandManager.RequerySuggested -= value;
         }
 
-        public bool CanExecute(object parameter) => canExecute == null ? true : canExecute(parameter);
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null ? true : canExecute((T)parameter);
+        }
 
-        public void Execute(object parameter) => executeAction(parameter);
+        public void Execute(object? parameter)
+        {
+            executeAction((T)parameter);
+        }
     }
+
+    //public class RelayCommand : ICommand
+    //{
+    //    Action<object> executeAction;
+    //    Func<object, bool> canExecute;
+    //    bool canExecuteCache;
+    //    public RelayCommand(Action<object> executeAction, Func<object, bool> canExecute, bool canExecuteCache)
+    //    {
+    //        this.canExecute = canExecute;
+    //        this.executeAction = executeAction;
+    //        this.canExecuteCache = canExecuteCache;
+    //    }
+    //    public event EventHandler CanExecuteChanged
+    //    {
+    //        add => CommandManager.RequerySuggested += value;
+    //        remove => CommandManager.RequerySuggested -= value;
+    //    }
+
+    //    public bool CanExecute(object parameter) => canExecute == null ? true : canExecute(parameter);
+
+    //    public void Execute(object parameter) => executeAction(parameter);
+    //}
 }
