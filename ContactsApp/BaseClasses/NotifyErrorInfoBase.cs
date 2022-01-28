6ad9aed42ebfc3447839bb43d1f14de5dbe4aa4b
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-28 16:28:23 +0530

    implemented base classes for Error and prop change notification

---
+++ ContactsApp/BaseClasses/NotifyErrorInfoBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace ContactsApp.BaseClasses
+{
+    public abstract class NotifyErrorInfoBase : INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _propErrors;
+
+        public bool HasErrors => _propErrors.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+
+        public NotifyErrorInfoBase()
+        {
+            _propErrors = new Dictionary<string, List<string>>();
+        }
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return _propErrors.GetValueOrDefault(propertyName, null);
+        }
+
+
+        protected void AddError(string errMsg, [CallerMemberName] string propName = "")
+        {
+            if (!_propErrors.ContainsKey(propName))
+            {
+                _propErrors.Add(propName, new List<string>());
+            }
+
+            _propErrors[propName].Add(errMsg);
+            OnErrorChanged(propName);
+        }
+
+        protected void RemoveError([CallerMemberName] String propName = "")
+        {
+            if (_propErrors.Remove(propName))
+            {
+                OnErrorChanged(propName);
+            }
+        }
+
+        private void OnErrorChanged(string propName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propName));
+        }
+    }
+}
---
+++ ContactsApp/BaseClasses/NotifyPropChangedBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ContactsApp.BaseClasses
+{
+    public abstract class NotifyPropChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChange([CallerMemberName] string propName = "")
+        {
+            if (PropertyChanged == null) return;
+            PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
